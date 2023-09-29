063ac1747c38b4490f91747d44d349af3d27d8e0
Prasad Jadhav <86917788+JadhavPrasad21@users.noreply.github.com>
2023-09-29 08:29:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	 const priceElements = document.querySelectorAll(".prices");
+
+    // Step 2: Calculate the total price
+    let totalPrice = 0;
+    priceElements.forEach((priceElement) => {
+        const price = parseFloat(priceElement.textContent); // Convert the price text to a number
+        if (!isNaN(price)) {
+            totalPrice += price;
+        }
+    });
+
+    // Step 3: Create a new row and cell to display the total price
+    const table = document.querySelector("table");
+    const newRow = table.insertRow(-1); // -1 appends the row to the end of the table
+    const newCell = newRow.insertCell(0);
+
+    // Display the total price in the new cell
+    newCell.textContent = "Total Price: " + totalPrice.toFixed(2); // Format to two decimal places
+};
   
 };
 
